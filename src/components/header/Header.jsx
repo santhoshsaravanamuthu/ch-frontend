630d6f4493aa70bd295b1e66add35b8f9c1ff295
santhoshsaravanamuthu <santhoshs.csbs2020@citchennai.net>
2023-03-09 20:58:38 +0530

    first code

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,41 +1,31 @@
 
 import { AppBar, Toolbar, styled} from '@mui/material'; 
 import { Link } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
-
 
 const Component = styled(AppBar)`
-    background: #FFFFFF;
+    background: #0d263b;
     color: black;
 `;
 
 const Container = styled(Toolbar)`
-    justify-content: right;
+    justify-content: center;
     & > a {
         padding: 20px;
-        color: #000;
+        color: #FFFFFF;
         text-decoration: none;
     }
 `
 
 const Header = () => {
     return (
-        <ThemeProvider theme={darkTheme}>
-            <Component>
-                <Container>
-                    <Link to='/'>HOME</Link>
-                    <Link to='/about'>ABOUT</Link>
-                    <Link to='/contact'>CONTACT</Link>
-                    <Link to='/account'>LOGOUT</Link>
-                </Container>
-            </Component>
-        </ThemeProvider>
+        <Component>
+            <Container>
+                <Link to='/'>HOME</Link>
+                <Link to='/about'>ABOUT</Link>
+                <Link to='/contact'>CONTACT</Link>
+                <Link to='/account'>LOGOUT</Link>
+            </Container>
+        </Component>
     )
 }
 
